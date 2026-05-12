09b20d3adab011c96aaa40507ef58eacce9e43e0
=QuangDev <=quangtruong0420@gmail.com>
2026-05-12 20:10:29 +0700

    Add_Resoure

---
+++ Thuchanh/Lab08/assets/js/main.js
@@ -0,0 +1,103 @@
+'use strict';
+
+(function ($) {
+
+    /*------------------
+        Preloader
+    --------------------*/
+    $(window).on('load', function () {
+        $(".loader").fadeOut();
+        $("#preloder").delay(200).fadeOut("slow");
+
+        /*------------------
+            Gallery filter
+        --------------------*/
+        $('.featured__controls li').on('click', function () {
+            $('.featured__controls li').removeClass('active');
+            $(this).addClass('active');
+        });
+        if ($('.featured__filter').length > 0) {
+            var containerEl = document.querySelector('.featured__filter');
+            var mixer = mixitup(containerEl);
+        }
+    });
+
+    /*------------------
+        Background Set
+    --------------------*/
+    $('.set-bg').each(function () {
+        var bg = $(this).data('setbg');
+        $(this).css('background-image', 'url(' + bg + ')');
+    });
+
+    //Humberger Menu
+    $(".humberger__open").on('click', function () {
+        $(".humberger__menu__wrapper").addClass("show__humberger__menu__wrapper");
+        $(".humberger__menu__overlay").addClass("active");
+        $("body").addClass("over_hid");
+    });
+
+    $(".humberger__menu__overlay").on('click', function () {
+        $(".humberger__menu__wrapper").removeClass("show__humberger__menu__wrapper");
+        $(".humberger__menu__overlay").removeClass("active");
+        $("body").removeClass("over_hid");
+    });
+
+    /*------------------
+		Navigation
+	--------------------*/
+    $(".mobile-menu").slicknav({
+        prependTo: '#mobile-menu-wrap',
+        allowParentLinks: true
+    });
+
+
+    $('.hero__categories__all').on('click', function(){
+        $('.hero__categories ul').slideToggle(400);
+    });
+
+    /*-----------------------
+        Categories Slider
+    ------------------------*/
+    $(".categories__slider").owlCarousel({
+        loop: true,
+
+        margin: 10,
+
+        // Hiển thị 1 ảnh mỗi lần
+        items: 1,
+
+        // Hiện chấm tròn (5 chấm ứng với 5 ảnh)
+        dots: true,
+
+        // Hiện nút next prev
+        nav: true,
+
+        // Chuyển đúng 1 slide mỗi lần
+        slideBy: 1,
+
+        navText: [
+            "<span>‹</span>",
+            "<span>›</span>"
+        ],
+
+        smartSpeed: 1000,
+
+        autoplay: true,
+
+        // Responsive: ảnh rộng ~460px
+        responsive: {
+            0: {
+                items: 1
+            },
+            480: {
+                items: 1
+            },
+            768: {
+                items: 1
+            }
+        }
+    });
+
+
+})(jQuery);
